ad3297705063d90ba315d84b56c384b0e22b7298
zidan <zidantaufan44@gmail.com>
2024-08-27 22:15:58 +0700

    initial commit

---
+++ twopoint/app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+@app.route('/messi')
+def endpoint2():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
